87eaf5e92f7af04ddfc7883ffa50b1eb9bb81675
Israel González de la Peña <israelgonzalez@MacBook-Pro.local>
2020-12-03 23:16:50 -0600

    Faq section created

---
+++ src/components/FaqSection.js
@@ -0,0 +1,39 @@
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How Do I Start?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Different Paymenth Methods</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What Producst Do You Offer?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
