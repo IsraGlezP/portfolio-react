d80572f392a53b5121c697911bdabda00955fa0c
Israel González de la Peña <israelgonzalez@MacBook-Pro.local>
2020-12-09 22:12:08 -0600

    Added links to Our Work and Contact Us pages

--- src/App.js
+++ src/App.js
@@ -3,14 +3,32 @@
 import GlobalStyle from "./components/GlobalStyle";
 // Import pages
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+import MovieDetail from "./pages/MovieDetail";
 import Nav from "./components/Nav";
+// Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work" exact>
+          <OurWork />
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetail />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
